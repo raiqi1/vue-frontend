b1e8b1b3865b420e684d7f9dbe2826723931376a
rayiqi <ramadifan769@gmail.com>
2024-11-02 07:21:57 +0700

    oko2

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,8 @@ import { adminRoutes } from './routes/adminRoutes'
 import { blogRoutes } from './routes/blogRoutes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
-  routes: [
-    authRoutes,
-    adminRoutes,
-    blogRoutes,
-  ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [authRoutes, adminRoutes, blogRoutes]
 })
 
 export default router
