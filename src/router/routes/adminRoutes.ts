94115e926a2cffc828e905b0dd45a56f38854963
rayiqi <ramadifan769@gmail.com>
2024-11-02 07:28:39 +0700

    okk

--- src/router/routes/adminRoutes.ts
+++ src/router/routes/adminRoutes.ts
@@ -1,24 +1,46 @@
+// export const adminRoutes= {
+//     path: '/admin',
+//     name: 'admin',
+//     component: () => import('../../views/admin/AdminPage.vue'),
+//     children: [
+//       {
+//         path: '/admin',
+//         name: 'admin',
+//         component: () => import('../../views/admin/dashbaord/DashboardPage.vue')
+//       },
+//       {
+//         path: '/create-post',
+//         name: 'create-post',
+//         component: () => import('../../views/admin/createPost/CreatePostPage.vue')
+//       },
+//       {
+//         path: '/post-lists',
+//         name: 'post-lists',
+//         component: () => import('../../views/admin/postList//PostListPage.vue')
+//       },
 
-export const adminRoutes= {
-    path: '/admin',
-    name: 'admin',
-    component: () => import('../../views/admin/AdminPage.vue'),
-    children: [
-      {
-        path: '/admin',
-        name: 'admin',
-        component: () => import('../../views/admin/dashbaord/DashboardPage.vue')
-      },
-      {
-        path: '/create-post',
-        name: 'create-post',
-        component: () => import('../../views/admin/createPost/CreatePostPage.vue')
-      },
-      {
-        path: '/post-lists',
-        name: 'post-lists',
-        component: () => import('../../views/admin/postList//PostListPage.vue')
-      },
-     
-    ]
-  }
+//     ]
+//   }
+
+export const adminRoutes = {
+  path: '/admin',
+  name: 'admin',
+  component: () => import('../../views/admin/AdminPage.vue'),
+  children: [
+    {
+      path: '', // Halaman default untuk /admin
+      name: 'admin',
+      component: () => import('../../views/admin/dashbaord/DashboardPage.vue')
+    },
+    {
+      path: 'create-post', // Akses ke /admin/create-post
+      name: 'create-post',
+      component: () => import('../../views/admin/createPost/CreatePostPage.vue')
+    },
+    {
+      path: 'post-lists', // Akses ke /admin/post-lists
+      name: 'post-lists',
+      component: () => import('../../views/admin/postList/PostListPage.vue')
+    }
+  ]
+}
